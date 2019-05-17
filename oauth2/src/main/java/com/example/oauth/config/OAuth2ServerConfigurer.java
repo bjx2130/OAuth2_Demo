83d637a8df8c4835e937d7e45183422a0658758c
bjx2130 <308110158@qq.com>
2019-05-17 09:20:01 +0800

    Update OAuth2ServerConfigurer.java

--- oauth2/src/main/java/com/example/oauth/config/OAuth2ServerConfigurer.java
+++ oauth2/src/main/java/com/example/oauth/config/OAuth2ServerConfigurer.java
@@ -10,15 +10,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  * @author bWX535804
  * 
  * 授权码流程：
- * 			1.请求：http://localhost:8080/oauth/authorize?client_id=client&response_type=code&redirect_uri=http://www.baidu.com
- * 			     重定向：https://www.baidu.com/?code=YICO47	
- * 			2.请求 ：http://localhost:8080/oauth/token 获取 access_token
- * 			     返回：{
- *					  "access_token": "32a1ca28-bc7a-4147-88a1-c95abcc30556", // 令牌
- *					  "token_type": "bearer",
- *					  "expires_in": 2591999,
- *					  "scope": "app"
- *					}
+ * 			1.GET请求：http://localhost:8888/oauth/authorize?client_id=client&response_type=code&redirect_uri=http://www.baidu.com
+ * 			     	重定向：https://www.baidu.com/?code=YICO47	
+ * 			2.POST请求 ：http://localhost:8888/oauth/token?code=5lMWwD&grant_type=authorization_code&redirect_uri=http://www.baidu.com&scope=app 
+ * 					添加header: Authorization:Basic 编码(client:secret)
+ * 											     		
+ * 						返回：{
+ *						    "access_token": "7989cf2d-331e-4e74-80c8-506b01cd4d5f",
+ *						    "token_type": "bearer",
+ *						    "expires_in": 43199,
+ *						    "scope": "app"
+ *						}
+ *
+ *			3.请求资源服务器：http://resource:port/hello?access_token=7989cf2d-331e-4e74-80c8-506b01cd4d5f
+ *
  *
  */
 
