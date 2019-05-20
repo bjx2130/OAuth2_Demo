b6f73923edc8324138c1e6599fc52419b2346588
bjx2130 <308110158@qq.com>
2019-05-20 10:51:56 +0800

    Update OAuth2ServerConfigurer.java

--- oauth2/src/main/java/com/example/oauth/config/OAuth2ServerConfigurer.java
+++ oauth2/src/main/java/com/example/oauth/config/OAuth2ServerConfigurer.java
@@ -1,64 +1,59 @@
 package com.example.oauth.config;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
-/**
- * 
- * @author bWX535804
- * 
- * 授权码流程：
- * 			1.GET请求：http://localhost:8888/oauth/authorize?client_id=client&response_type=code&redirect_uri=http://www.baidu.com
- * 			     	重定向：https://www.baidu.com/?code=YICO47	
- * 			2.POST请求 ：http://localhost:8888/oauth/token?code=5lMWwD&grant_type=authorization_code&redirect_uri=http://www.baidu.com&scope=app 
- * 					添加header: Authorization:Basic 编码(client:secret)
- * 											     		
- * 						返回：{
- *						    "access_token": "7989cf2d-331e-4e74-80c8-506b01cd4d5f",
- *						    "token_type": "bearer",
- *						    "expires_in": 43199,
- *						    "scope": "app"
- *						}
- *
- *			3.请求资源服务器：http://resource:port/hello?access_token=7989cf2d-331e-4e74-80c8-506b01cd4d5f
- *
- *
- */
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2ServerConfigurer extends AuthorizationServerConfigurerAdapter {
-
+	
+	@Autowired
+	private BCryptPasswordEncoder bcryptPasswordEncoder;
 	
 	/**
-	 * 
-	 * =======可选配置=========
-	 * 
-	 * 资源服务器与认证服务器分离时
-	 * 例如资源服务器 ResourceServerTokenServices 的  RemoteTokenServices 实现类时
-	 * 需要授权资源服务器权限：http://localhost:8888/oauth/check_token
+	 * 在spring5.0之后，springsecurity client密码 和 user密码必须用 PasswordEncoder 加密
+	 * @return
 	 */
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
-		
-	}
-	
-	
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder(){
+    	return new BCryptPasswordEncoder();
+    }
+    
+    
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory() // 使用in-memory存储
 				.withClient("client") // client_id
-				.secret("secret") // client_secret
+				.secret(bcryptPasswordEncoder.encode("secret")) // client_secret
 				.authorizedGrantTypes("authorization_code") // 该client允许的授权类型
 				// .autoApprove(true) //登录后绕过批准询问(/oauth/confirm_access)
 				.redirectUris("http://www.baidu.com")
 				.scopes("app"); // 允许的授权范围
 
 	}
-	
+    
+    
+	/**
+	 * =======可选配置=========
+	 * 资源服务器与认证服务器分离时
+	 * 例如资源服务器 ResourceServerTokenServices 的  RemoteTokenServices 实现类时
+	 * 需要授权资源服务器权限：http://localhost:8888/oauth/check_token
+	 */
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security
+			.tokenKeyAccess("permitAll()")
+			.checkTokenAccess("isAuthenticated()")
+			.allowFormAuthenticationForClients();
+		
+	}
 
 }
+
