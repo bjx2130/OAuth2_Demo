4c3ac3b21222efcdad01ff5c56fc512eeb13916a
bjx2130 <308110158@qq.com>
2019-05-20 10:52:39 +0800

    Update WebSecurityConfiguration.java

--- oauth2/src/main/java/com/example/oauth/config/WebSecurityConfiguration.java
+++ oauth2/src/main/java/com/example/oauth/config/WebSecurityConfiguration.java
@@ -1,16 +1,21 @@
 package com.example.oauth.config;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	
+	@Autowired
+	private BCryptPasswordEncoder bcryptPasswordEncoder;
+	
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -23,11 +28,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		// 在spring5.0之后，springsecurity存储密码的格式发生了改变
-		// 官方文档
 		// https://docs.spring.io/spring-security/site/docs/5.0.7.BUILD-SNAPSHOT/reference/htmlsingle/
-		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("admin")
-				.password(new BCryptPasswordEncoder().encode("111")).roles("USER").and().withUser("admin2")
-				.password(new BCryptPasswordEncoder().encode("222")).roles("ADMIN");
+		auth.inMemoryAuthentication().passwordEncoder(bcryptPasswordEncoder).withUser("admin")
+				.password(bcryptPasswordEncoder.encode("111")).roles("USER").and().withUser("admin2")
+				.password(bcryptPasswordEncoder.encode("222")).roles("ADMIN");
     }
+    
+
 }
